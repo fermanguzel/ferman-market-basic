f4cc24a354772078025d7ddf7b47a03624e3e200
fermanguzel <fermanguzel@gmail.com>
2023-09-22 21:36:51 +0300

    ferman

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
-let NotFound = () => {
-
+const NotFound = () => {
     useEffect(() => {
         document.title = "404 - Ferman Market";
     }, []);
 
     return (
-        <div className="text-danger">
-            <h1>404 - Page Not Found</h1>
+        <div className="text-center mt-5">
+            <h1 className="text-danger">404 - Page Not Found</h1>
+            <p className="text-muted">The page you are looking for does not exist.</p>
         </div>
-    )
-}
+    );
+};
 
-export default NotFound
+export default NotFound;
